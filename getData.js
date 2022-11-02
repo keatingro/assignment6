9a2afaf5d83eba55c4187800adace20a22b7b4ed
Keating <keatingrowe@gmail.com>
2022-11-01 22:54:33 -0700

    Add files via upload

---
+++ getData.js
@@ -0,0 +1,11 @@
+
+$.getJSON('http://api.open-notify.org/astros.json?callback=?', function (data) {
+    var number = data['number'];
+    $('#SpacePeople').html(number);
+
+    data['people'].forEach(function (d) {
+        $('#astroNames').append('<li>' + d['name'] + '</li>');
+    });
+});
+
+t
